95f4754dc03889a91c73e2c73bc7787ece507c05
Vladimir <kiryanov_vova_134@mail.ru>
2023-05-12 02:15:17 +0300

    Redefined indexers for Stack class with Get and Set methods.

--- algorithms/algorithms/Program.cs
+++ algorithms/algorithms/Program.cs
@@ -1,28 +1,41 @@
 ﻿using algorithms;
 
+
 // --- Работа со стеком --- //
 
 var st = new algorithms.Stack();
-Console.WriteLine(st.Show());
+
+Console.WriteLine(st.Show());        // HEAD -> {  }
 st.Push(777);
-Console.WriteLine(st.Show());
 st.Push(69);
 st.Push(1337);
-Console.WriteLine(st.Show());
-Console.WriteLine(st.Pop().Value);
-Console.WriteLine(st.Show());
-Console.WriteLine(st.Pop().Value);
-Console.WriteLine(st.Show());
-Console.WriteLine(st.Pop().Value);
-Console.WriteLine(st.Show());
+Console.WriteLine(st.Show());        // HEAD -> { 1337, 69, 777 }
+
+Console.WriteLine(st[1]);            // 69
+// Console.WriteLine(st[5]);         // *ВЫЛЕТИТ ИСКЛЮЧЕНИЕ*
+// Console.WriteLine(st[-1]);        // *ВЫЛЕТИТ ИСКЛЮЧЕНИЕ*
+
+st[1] = 666;
+Console.WriteLine(st[1]);            // 666
+// Console.WriteLine(st[-1]);        // *ВЫЛЕТИТ ИСКЛЮЧЕНИЕ*
+// Console.WriteLine(st[4]);         // *ВЫЛЕТИТ ИСКЛЮЧЕНИЕ*
+Console.WriteLine(st.Show());        // HEAD-> { 1337, 666, 69, 777 }
+
+Console.WriteLine(st.Pop().Value);   // 1337
+Console.WriteLine(st.Pop().Value);   // 666
+Console.WriteLine(st.Pop().Value);   // 69
+Console.WriteLine(st.Pop().Value);   // 777
+Console.WriteLine(st.Show());        // HEAD -> {  }
 Console.WriteLine("\n\n\n");
+
+
 
 // --- Сортировка --- //
 
 int[] arr = new int[] { 5, 0, 1, 37, 88, -12, -1000, 25 };
 Console.WriteLine("До сортировки: ");
-foreach (int i in arr) { Console.Write(i + " "); }
+foreach (int i in arr) { Console.Write(i + " "); }              //   5 0 1 37 88 -12 -1000 25
 SimpleChoice sc = new SimpleChoice(arr);
 Console.WriteLine("\nПосле сортировки: ");
-foreach (int i in sc.Sort()) { Console.Write(i + " "); }
+foreach (int i in sc.Sort()) { Console.Write(i + " "); }        //   -1000 -12 0 1 5 25 37 88
 Console.WriteLine("\n\n\n");
--- algorithms/algorithms/Stack.cs
+++ algorithms/algorithms/Stack.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,13 @@ namespace algorithms
             this.length++;
         }
 
+        public void Push(Element elem)
+        {
+            elem.Next = head;
+            head = elem;
+            this.length++;
+        }
+
         public Element? Pop()
         {
             if (head != null)
@@ -37,6 +45,72 @@ namespace algorithms
             {
                 throw new Exception("Stack is empty.");
             }
+        }
+
+        public int Get(int index)
+        {
+            if ((this.length - 1 >= index) && (index >= 0))
+            {
+                Stack tmp_stack = new Stack();
+                for (int cnt = 0; cnt < index; cnt++)
+                {
+                    tmp_stack.Push(this.Pop());
+                }
+                int result = this.head.Value;
+                for (int tmp_cnt = 0; tmp_cnt < tmp_stack.length; tmp_cnt++)
+                {
+                    this.Push(tmp_stack.Pop());
+                }
+                return result;
+            }
+            throw new Exception("Index out of range.");  
+        }
+
+        public void Set(int index, int value)
+        {
+            if ((index >= 0) && (index < this.length))
+            {
+                Stack tmp_stack = new Stack();
+                for (int cnt = 0; cnt < index; cnt++)
+                {
+                    tmp_stack.Push(this.Pop());
+                }
+                this.Push(new Element(value, this.head));
+                if (tmp_stack.length > 0)
+                {
+                    for (int tmp_cnt = 0; tmp_cnt < tmp_stack.length + 1; tmp_cnt++)
+                    {
+                        this.Push(tmp_stack.Pop());
+                    }
+                }
+                this.length++;
+                return;
+            }
+            throw new Exception("Index out of range.");
+        }
+
+        public void Set(int index, Element value)
+        {
+            if ((index >= 0) && (index < this.length))
+            {
+                Stack tmp_stack = new Stack();
+                for (int cnt = 0; cnt < index; cnt++)
+                {
+                    tmp_stack.Push(this.Pop());
+                }
+                value.Next = this.head;
+                this.Push(value);
+                if (tmp_stack.length > 0)
+                {
+                    for (int tmp_cnt = 0; tmp_cnt < tmp_stack.length + 1; tmp_cnt++)
+                    {
+                        this.Push(tmp_stack.Pop());
+                    }
+                }
+                this.length++;
+                return;
+            }
+            throw new Exception("Index out of range.");
         }
 
         public string Show()
@@ -55,6 +129,12 @@ namespace algorithms
             }
             return result + " }";
 
+        }
+
+        public int this[int index]
+        {
+            get => Get(index);
+            set => Set(index, value);
         }
     }
 }
